e6b6717fdb50b0698341241963bd84ab45bec2f3
victoriawanko-create. <victoriawanko@gmail.com.>
2026-07-14 09:49:36 -0400

    home page done

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, {useRef, useEffect} from 'react'
 import "./TitleCards.css"
+import cards_data from '../../assets/cards/Cards_data'
 
-const TitleCards = () => {
+const TitleCards = ({title, category}) => {
+
+    const cardRef = useRef();
+
+    const handleWheel = (event) => {
+        event.preventDefault;
+        cardRef.current.scrollLeft += event/deltaY;
+    }
+
+    useEffect(() => {
+        cardRef.current.addEventListener('wheel', handleWheel)
+    }, [])
+
     return (
-        <div className="titlecards">
-
+        <div className="title-cards">
+            <h2>{title?title:'Popular on Netflix'}</h2>
+            <div className="card-list" ref={cardRef}>
+                {cards_data.map((card, index) => {
+                    return <div className="card" key={index}>
+                       <img src={card.image} alt=""></img>
+                       <p>{card.name}</p>
+                        </div>
+                    
+                })}
+            </div>
         </div>
     )
 }
